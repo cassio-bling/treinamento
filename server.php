261249a873cab04f7c6a048db703f40a872d477e
Cassio Perin <cassio.perin@bling.com.br>
2019-11-12 14:03:30 -0300

    First Commit

---
+++ server.php
@@ -0,0 +1,11 @@
+<?php
+
+function teste() : array {
+    return [
+        ["id" => 1, "modelo" => "adwwdadawawdaw"],
+        ["id" => 1, "modelo" => "zxczxczxcx"]
+    ];
+}
+//error_log(json_encode(teste()));
+
+//echo json_encode(teste());
